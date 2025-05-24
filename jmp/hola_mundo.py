9288f97d508d69432f21cd1a904ff7e6a764b06c
Miranda Perez pacheco <miri@MacBook-Air-de-Miranda.local>
2025-05-23 23:45:31 -0600

    se añaden instrucciones

--- jmp/hola_mundo.py
+++ jmp/hola_mundo.py
@@ -1,1 +1,9 @@
 print("Hola mundo")
+#* Touch es para añadir un archivo
+# * git add . es para añadir todos los archivos
+# * git commit -m "mensaje" es para hacer un commit
+# * git push es para subir los cambios al repositorio remoto
+# git status es para ver el estado de los archivos ** sirve para ver que paso sigue 
+# mkdir es para crear una carpeta
+# cd es para entrar a una carpeta
+# cd ../ es para regresar a la carpeta anterior
